32a912f996f128d5c114347d24f47f323d44a2d8
”mana-nan” <mananan0728@icloud.com>
2020-09-18 08:22:26 +0000

    php03課題

---
+++ 03homework.php
@@ -0,0 +1,40 @@
+<?php
+#1
+$name = "まな";
+if ($name == "まな") {
+    echo "私は" . $name . "です";
+} else {
+    echo "私は" . $name . "ではありません";
+}
+echo "\n";
+
+#2
+$total = 0;
+for ($i = 1; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+#3
+$fruits = array ("ぶどう","マスカット","マンゴー","パッション","いちご");
+foreach ($fruits as $fruit) {
+    echo "食べたいのは" . $fruit;
+    echo "\n";
+}
+
+#4
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
+
+?>
